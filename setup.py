c41ad95553e49819f31f9eed75f24b9dbbccff33
siwuxei <50586809+siwuxei@users.noreply.github.com>
2020-08-17 03:34:48 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ pip install numpy
 pip install pandas
 pip install matplotlib
 pip install scipy
-pip scikit-learn
+pip install scikit-learn
 pip install pyecharts
 pip install seaborn
 pip install plotly
