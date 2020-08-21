e0515360ae4cfde7f1d6f647f6a4d10d0e7b5c91
siwuxei <1757579379@qq.com>
2020-08-22 02:26:02 +0800

    1

--- Notebook/2020-08-21-sklean.py
+++ Notebook/2020-08-21-sklean.py
@@ -1,9 +1,13 @@
 #导入包
 from sklearn.feature_extraction import DictVectorizer
+
 def dictvec():
-    #实例化
-    dict=DictVectorizer()
-    #调用fit_trandform
-    dict.fit_transform([{'city':'北京','temperature':100}{''city':'上海','temperature':60'}...
-                        {'city':'深圳','temperature':30}])
-    
+    # 实例化
+    dict = DictVectorizer()
+    # 调用fit_transform
+    data = dict.fit_transform([{'city': '北京', 'temperature': 100},{'city': '上海', 'temperature': 60},{'city': '深圳', 'temperature': 30}])
+    print(data)
+    return None
+
+if __name__ == "__main__":
+    dictvec()
